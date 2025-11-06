fc881d2a5b59bb05bc504addad61870fc99df3ee
karan <xkaranx5@gmail.com>
2025-11-06 13:26:26 +0530

    feat: update API key handling in location fetching function

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const lat = 28.6532
 const lon = 77.2108
+const API_KEY ="your_api_key_here"
 
 function location() {
-    fetch(`https://us1.locationiq.com/v1/reverse?key=pk.afe408bf175dc1ac9bf57badd656a9a0&lat=${lat}&lon=${lon}&format=json&`)
+    fetch(`https://us1.locationiq.com/v1/reverse?key=${API_KEY}&lat=${lat}&lon=${lon}&format=json&`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
